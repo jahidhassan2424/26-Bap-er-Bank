db910458971aec36a8af03db0f961262d28a72c5
jahidhassan2424 <jahidhassan.programmer@gmail.com>
2022-02-11 10:17:14 +0600

    Login/New cus button working perfectily

--- js/login.js
+++ js/login.js
@@ -11,3 +11,22 @@ loginSubmitBtn.addEventListener('click', function () {
         console.log('Invalid Email or password!');
     }
 })
+
+// Login Form
+function loginFormAppear() {
+    document.getElementById('login-form').style.display = 'block';
+    document.getElementById('login-btn').style.display = 'none';
+    document.getElementById('new-customer-form').style.display = 'none';
+    document.getElementById('new-customer-btn').style.display = 'inline';
+    // document.getElementById('invalid-popup').removeAttribute = 'style';
+    document.getElementById('invalid-popup').style.display = 'block';
+}
+
+// New Customer
+document.getElementById('new-customer-btn').addEventListener('click', function () {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('login-btn').style.display = 'inline';
+    document.getElementById('new-customer-form').style.display = 'block';
+    document.getElementById('new-customer-btn').style.display = 'none';
+    document.getElementById('invalid-popup').style.display = 'none';
+})
